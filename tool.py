c65f8d6a3d6d10b64fb6c63629deefaf1f4e5f77
g1pd78 <g1pd78@gmail.com>
2022-03-13 10:17:59 -1000

    edit and delete

--- tool.py
+++ tool.py
@@ -25,9 +25,17 @@ Base.metadata.create_all(engine)
 class FlashCards:
 
     msg_question = 'Question:'
-    msg_need_answer = 'Please press "y" to see the answer or press "n" to skip:'
+    msg_need_answer = 'press "y" to see the answer:'
     msg_answer = 'Answer:'
     msg_no_cards = 'There is no flashcard to practice!'
+    msg_update = 'press "u" to update:'
+    msg_skip = 'press "n" to skip:'
+    msg_current_question = 'current question:'
+    msg_new_question = 'please write a new question:'
+    msg_current_answer = 'current answer:'
+    msg_new_answer = 'please write a new answer:'
+    msg_delete = 'press "d" to delete the flashcard:'
+    msg_edit = 'press "e" to edit the flashcard:'
 
     def add_card(self):
         print(self.msg_question)
@@ -49,13 +57,55 @@ class FlashCards:
 
     def practice_card(self):
         res = session.query(CardsTable).all()
-        for i in res:
-            print(self.msg_question, i.question)
-            print(self.msg_need_answer)
-            if input() == 'y':
+
+        def read_option(i):
+            option = input()
+
+            def read_in_option(i):
+                in_opt = input()
+                if in_opt == 'd':
+                    session.query(CardsTable).filter(CardsTable.id == i.id).delete()
+                    session.commit()
+                elif in_opt == 'e':
+                    filter = session.query(CardsTable).filter(CardsTable.id == i.id)
+                    print(self.msg_current_question, i.question)
+                    print(self.msg_new_question)
+                    filter.update({"question": input()})
+                    print(self.msg_current_answer, i.answer)
+                    print(self.msg_new_answer)
+                    filter.update({"answer": input()})
+                    session.commit()
+                else:
+                    print(f'{option} is not an option')
+                    return False
+                return True
+
+            if option == 'y':
                 print()
                 print(self.msg_answer, i.answer)
-            print()
+            elif option == 'n':
+                pass
+            elif option == 'u':
+                print(self.msg_delete)
+                print(self.msg_edit)
+                while not read_in_option(i):
+                    pass
+            else:
+                print(f'{option} is not an option')
+                return False
+            return True
+
+        if not res:
+            print(self.msg_no_cards)
+        else:
+            for i in res:
+                print(self.msg_question, i.question)
+                print(self.msg_need_answer)
+                print(self.msg_skip)
+                print(self.msg_update)
+                while not read_option(i):
+                    pass
+                print()
 
 
 class Menu:
